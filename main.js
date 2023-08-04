123b83968e0100e933ae7b196ab89595d0037962
ezequielmagi <ezequielmaggi@gmail.com>
2023-08-04 00:47:39 -0300

    finalizo el header

---
+++ main.js
@@ -0,0 +1,6 @@
+//Declaraciones
+const hamButton = document.getElementById('hamButton')
+
+hamButton.addEventListener( 'click', () =>{
+    hamButton.classList.toggle('nav-hamburger_button--open')
+} )
